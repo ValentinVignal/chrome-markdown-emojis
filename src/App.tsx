06d60cf095ce15f1a28c77bdc50913c541759e59
ValentinVignal <valentin.vignal.dev@outlook.fr>
2021-08-28 19:05:12 +0800

    :sparkles: Save the enabled settings

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,9 @@
-import { createTheme, CssBaseline, ThemeProvider, useMediaQuery } from '@material-ui/core';
-import Switch from '@material-ui/core/Switch';
-import React from 'react';
+import { createTheme, CssBaseline, Switch, ThemeProvider, useMediaQuery } from '@material-ui/core';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-  // const [title, setTitle] = React.useState('');
-  // const [headlines, setHeadlines] = React.useState<string[]>([]);
-
-  // React.useEffect(() => {
-  //   /**
-  //    * We can't use "chrome.runtime.sendMessage" for sending messages from React.
-  //    * For sending messages from React we need to specify which tab to send it to.
-  //    */
-  //   // chrome.tabs && chrome.tabs.query({
-  //   //   active: true,
-  //   //   currentWindow: true
-  //   // }, tabs => {
-  //   //   /**
-  //   //    * Sends a single message to the content script(s) in the specified tab,
-  //   //    * with an optional callback to run when a response is sent back.
-  //   //    *
-  //   //    * The runtime.onMessage event is fired in each content script running
-  //   //    * in the specified tab for the current extension.
-  //   //    */
-  //   //   chrome.tabs.sendMessage(
-  //   //     tabs[0].id || 0,
-  //   //     { type: 'GET_DOM' } as DOMMessage,
-  //   //     (response: DOMMessageResponse) => {
-  //   //       setTitle(response?.title ?? '');
-  //   //       setHeadlines(response?.headlines ?? []);
-  //   //     });
-  //   // });
-  // });
-
   const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
-
   const theme = React.useMemo(
     () =>
       createTheme({
@@ -93,17 +62,38 @@ function Body() {
   return (
     <div>
       <h1>Chrome Markdown emojis</h1>
-      <div className='row'>
-        <p>Enabled</p>
-        <Switch
-          checked={true}
-          name="switchEnabled"
-          onChange={(_) => { }}
-        />
-      </div>
-
+      <EnabledSwitch />
     </div>
 
   );
 }
 
+
+function EnabledSwitch() {
+  const [enabled, setEnabled] = useState<boolean | null>(null);
+
+
+  useEffect(() => {
+    chrome.storage.sync.get('enabled', (data) => {
+      console.log(data);
+      setEnabled(data.enabled);
+    });
+  });
+
+
+  return <div className='row'>
+    <p>Enabled</p>
+    <Switch
+      checked={enabled ?? true}
+      name="switchEnabled"
+      onChange={(event) => {
+        if (enabled === null) {
+          // It was not loaded
+          return;
+        }
+        chrome.storage.sync.set({ enabled: event.target.checked });
+        setEnabled(event.target.checked);
+      }}
+    />
+  </div>;
+}
