d41de9e6282023d99c1a8ebdf9a52045167a7eb8
ValentinVignal <valentin.vignal.dev@outlook.fr>
2024-02-18 14:00:00 +0100

    :art: Format responseCallback.ts

--- src/content/responseCallbacks.ts
+++ src/content/responseCallbacks.ts
@@ -4,24 +4,27 @@ import { globals } from "./globals";
 import { replaceEmoji } from "./handleText";
 
 /**
- * 
- * @param text 
- * @param response 
- * @returns 
+ *
+ * @param text
+ * @param response
+ * @returns
  */
-export function parseFullEmojiResponseCallback(response: ParseFullEmojiResponse): void {
+export function parseFullEmojiResponseCallback(
+  response: ParseFullEmojiResponse
+): void {
   if (response?.key && response.emoji && response.key !== response.emoji) {
     replaceEmoji(`:${response.key}: `, `${response.emoji} `);
   }
 }
 
-
 /**
  * @description Callback called when the partial emoji response is received
- * @param text 
- * @param response 
+ * @param text
+ * @param response
  */
-export function partialEmojiResponseCallback(response: PartialEmojiResponse): void {
+export function partialEmojiResponseCallback(
+  response: PartialEmojiResponse
+): void {
   globals.emojis = response.emojis ?? {};
   rebuildDropdown();
 }
