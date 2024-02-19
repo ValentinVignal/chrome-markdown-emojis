6dd9e70b75e9361b18e7699426a5eeafed0183a0
ValentinVignal <valentin.vignal.dev@outlook.fr>
2024-02-19 17:52:41 +0800

    :mute: Remove logs

--- src/content/onKeyUp.ts
+++ src/content/onKeyUp.ts
@@ -25,7 +25,6 @@ export function onKeyUp(event: KeyboardEvent): void {
   }
 
   const _target = event.target;
-  console.log("target", _target);
 
   if (!_target || !(_target instanceof HTMLElement)) {
     globals.target = null;
@@ -33,7 +32,6 @@ export function onKeyUp(event: KeyboardEvent): void {
   }
   globals.target = _target;
   const newText = getText(globals.target); // The text to work on
-  console.log("newText", newText);
   if (newText === globals.text) {
     // No change
     return;
@@ -67,7 +65,6 @@ export function onKeyUp(event: KeyboardEvent): void {
         }
       );
     } else if (couldBePartialEmoji && globals.settings.dropdownEnabled) {
-      console.log("partialEmojiMatch", partialEmojiMatch[0].split(":")[1]);
       chrome.runtime.sendMessage<Message, PartialEmojiResponse>(
         {
           type: MessageTypes.PartialEmoji,
