4409f6204d5c861bf00742ca97264dd27646ecdd
ValentinVignal <valentin.vignal.dev@outlook.fr>
2024-06-09 23:08:27 +0800

    :fire: Delete comment

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,7 @@ import reportWebVitals from './reportWebVitals';
 
 
 const container = document.getElementById('root')!;
-const root = createRoot(container); // createRoot(container!) if you use TypeScript
+const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <App />
